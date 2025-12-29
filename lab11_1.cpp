70d2909441c733d59ed7da51060c2c8d3b1a6237
pakin22326 <pakinounruen@gmail.com>
2025-12-30 01:18:50 +0700

    lab1 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    string input;
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; i++) {
+        getline(cin, input);
+    }
+
+    srand(time(0));
+    int r = rand() % 9;
+
+    cout << "You will get " << grades[r] << " in this 261102." << endl;
+
+    return 0;
+}
